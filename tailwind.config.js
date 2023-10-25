6d9bd1a5e5f3ae8082dcbef0228608e1179dcf58
Iván García <garcia.ballesteros96@gmail.com>
2023-10-25 18:01:41 +0200

    refactor some details & add cv download button

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
@@ -6,16 +7,39 @@ export default {
 			colors: {
 				igarcia: {
 					orange: {
-						50: '#FFE9AF',
-						100: '#FFC837',
-						200: '#FF8008'
+						100: '#FFFAEB',
+						200: '#FFF4D7',
+						300: '#FFEFC3',
+						400: '#FFE9AF',
+						500: '#FFE49B',
+						600: '#FFDE87',
+						700: '#FFD973',
+						800: '#FFD35F',
+						900: '#FFCE4B',
+						1000: '#FFC837'
 					},
 					yellow: {
-						100: '#fff601'
+						100: '#FFFEE6',
+						200: '#FFFDCC',
+						300: '#FFFCB3',
+						400: '#FFFB99',
+						500: '#FFFB80',
+						600: '#FFFA67',
+						700: '#FFF94D',
+						800: '#FFF834',
+						900: '#FFF71A',
+						1000: '#FFF601'
 					},
 					purple: {
-						100: '#8227FE',
-						200: '#D676EA'
+						0: '#E6D4FF',
+						100: '#CDA9FF',
+						200: '#B47DFE',
+						300: '#9B52FE',
+						400: '#8227FE',
+						500: '#681FCB',
+						600: '#4E1798',
+						700: '#341066',
+						800: '#1A0833'
 					}
 				}
 			},
@@ -29,13 +53,16 @@ export default {
 						opacity: 0
 					},
 					'66%': {
-						opacity: 0.4
+						opacity: 0.2
 					},
 					'100%': {
 						transform: 'translateY(-150px)',
 						opacity: 0
 					}
 				}
+			},
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans]
 			}
 		}
 	},
